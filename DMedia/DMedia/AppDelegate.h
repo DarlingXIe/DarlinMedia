f0a83fac9c0b3395b555341afcb8b1022b8c87be
DalinXie <xdl.tmac@qq.com>
2016-10-26 19:57:09 -0400

    updateVerBasic

---
+++ DMedia/DMedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DMedia
+//
+//  Created by DalinXie on 16/10/26.
+//  Copyright © 2016年 itdarlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DMedia/DMedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DMedia
+//
+//  Created by DalinXie on 16/10/26.
+//  Copyright © 2016年 itdarlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
